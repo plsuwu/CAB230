6bb82d88232ef35c8acf1edc5f017591c77be6e2
plsuwu <pls@plsuwu.com>
2024-03-28 04:26:21 +1000

    unsaved changes

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
-import NavigationD from "../components/NavigationD";
+import NavigationD from '../components/NavigationD';
 
 export default function Root() {
-    return (
-        <>
-            <div>
-                <NavigationD />
-                <Outlet />
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div>
+				<NavigationD />
+				<Outlet />
+			</div>
+		</>
+	);
 }
